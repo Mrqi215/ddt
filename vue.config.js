6ebb29cf08997c4442d80b30c4f3b83724448bfc
Mrqi <865235689@qq.com>
2022-12-12 16:05:22 +0800

    feature 增加图标修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,53 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        "appId": "com.app.mrQiDdt",
+        "productName": "ddtFormula",
+        "copyright": "Copyright © 2021 mrqi",
+        "directories": {
+          "buildResources": "build"
+        },
+        "mac": {
+          "category": "public.app-category.utilities"
+        },
+        "dmg": {
+          "background": "build/background.jfif",
+          "icon": "build/icons/icon.icns",
+          "iconSize": 100,
+          "contents": [
+            {
+              "x": 380,
+              "y": 180,
+              "type": "link",
+              "path": "/Applications"
+            },
+            {
+              "x": 130,
+              "y": 180,
+              "type": "file"
+            }
+          ],
+          "window": {
+            "width": 540,
+            "height": 380
+          }
+        },
+        "win": {
+          "target": [
+            "msi",
+            "nsis"
+          ],
+          "icon": "build/icons/icon.ico"
+        },
+        "nsis": {
+          "oneClick": false,
+          "language": "2052",
+          "perMachine": true,
+          "allowToChangeInstallationDirectory": true
+        }
+      }
+    }
+  }
 }
